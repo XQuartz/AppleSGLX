f44b48160210960d33fa93115407c557397d0ba5
gstaplin@apple.com <gstaplin@apple.com@9052f27e-8171-453d-b9bb-b0e8541e09c8>
2009-02-11 20:17:53 +0000

    Add the new pbuffer_destroy test to the target list.
    git-svn-id: http://svn.macosforge.org/repository/xquartz/AppleSGLX/trunk@121 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- tests/tests.mk
+++ tests/tests.mk
@@ -18,7 +18,7 @@ include tests/triangle_glx_single/triangle_glx.mk
 
 tests: $(TEST_BUILD_DIR)/simple $(TEST_BUILD_DIR)/fbconfigs $(TEST_BUILD_DIR)/triangle_glx \
   $(TEST_BUILD_DIR)/create_destroy_context $(TEST_BUILD_DIR)/glxgears $(TEST_BUILD_DIR)/glxinfo \
-  $(TEST_BUILD_DIR)/pbuffer \
+  $(TEST_BUILD_DIR)/pbuffer $(TEST_BUILD_DIR)/pbuffer_destroy \
   $(TEST_BUILD_DIR)/glxpixmap \
   $(TEST_BUILD_DIR)/triangle_glx_single
 
