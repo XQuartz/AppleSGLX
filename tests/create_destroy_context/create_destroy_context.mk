b875ef1c608f45227f1451d4740ca1beaa1573cd
gstaplin@apple.com <gstaplin@apple.com@9052f27e-8171-453d-b9bb-b0e8541e09c8>
2009-02-13 05:56:30 +0000

    Improve the test suite to weed out more bugs.  This particular test brings out a memory leak.
    git-svn-id: http://svn.macosforge.org/repository/xquartz/AppleSGLX/trunk@132 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- tests/create_destroy_context/create_destroy_context.mk
+++ tests/create_destroy_context/create_destroy_context.mk
@@ -5,3 +5,8 @@ $(TEST_BUILD_DIR)/create_destroy_context: tests/create_destroy_context/create_de
 $(TEST_BUILD_DIR)/create_destroy_context_alone: tests/create_destroy_context/create_destroy_context_alone.c
 	$(CC) tests/create_destroy_context/create_destroy_context_alone.c -Iinclude \
     -o $(TEST_BUILD_DIR)/create_destroy_context_alone $(LINK_TEST)
+
+$(TEST_BUILD_DIR)/create_destroy_context_with_drawable: tests/create_destroy_context/create_destroy_context_with_drawable.c
+	$(CC) tests/create_destroy_context/create_destroy_context_with_drawable.c -Iinclude \
+    -o $(TEST_BUILD_DIR)/create_destroy_context_with_drawable $(LINK_TEST)
+
--- tests/tests.mk
+++ tests/tests.mk
@@ -21,5 +21,6 @@ tests: $(TEST_BUILD_DIR)/simple $(TEST_BUILD_DIR)/fbconfigs $(TEST_BUILD_DIR)/tr
   $(TEST_BUILD_DIR)/pbuffer $(TEST_BUILD_DIR)/pbuffer_destroy \
   $(TEST_BUILD_DIR)/glxpixmap \
   $(TEST_BUILD_DIR)/triangle_glx_single \
-  $(TEST_BUILD_DIR)/create_destroy_context_alone
+  $(TEST_BUILD_DIR)/create_destroy_context_alone \
+  $(TEST_BUILD_DIR)/create_destroy_context_with_drawable
 
