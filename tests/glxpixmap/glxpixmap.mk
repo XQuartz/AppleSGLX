036e8a6fc79adfb0eaf31232a1715d80d4a87519
gstaplin@apple.com <gstaplin@apple.com@9052f27e-8171-453d-b9bb-b0e8541e09c8>
2009-02-19 04:22:27 +0000

    Add create/destroy test for GLXPixmaps.
    git-svn-id: http://svn.macosforge.org/repository/xquartz/AppleSGLX/trunk@170 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- tests/glxpixmap/glxpixmap.mk
+++ tests/glxpixmap/glxpixmap.mk
@@ -1,2 +1,6 @@
 $(TEST_BUILD_DIR)/glxpixmap: tests/glxpixmap/glxpixmap.c $(LIBGL)
 	$(CC) tests/glxpixmap/glxpixmap.c -Iinclude -I/usr/X11/include -o $(TEST_BUILD_DIR)/glxpixmap $(LINK_TEST)
+
+$(TEST_BUILD_DIR)/glxpixmap_create_destroy: tests/glxpixmap/glxpixmap_create_destroy.c $(LIBGL)
+	$(CC) tests/glxpixmap/glxpixmap_create_destroy.c -Iinclude -I/usr/X11/include -o $(TEST_BUILD_DIR)/glxpixmap_create_destroy $(LINK_TEST)
+
