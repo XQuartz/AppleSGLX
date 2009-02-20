ca84179a8985dbbe5f925e0672a23de2e5ea8357
gstaplin@apple.com <gstaplin@apple.com@9052f27e-8171-453d-b9bb-b0e8541e09c8>
2009-02-20 22:32:03 +0000

    Add a test for the GLXPixmap code that destroys an invalid GLXPixmap id.
    It doesn't pass at the moment.
    
    I think I will fix it in the future by making apple_glx_pixmap_destroy
    return a bool and using the XError pattern based on that.
    
    
    git-svn-id: http://svn.macosforge.org/repository/xquartz/AppleSGLX/trunk@211 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- tests/glxpixmap/glxpixmap.mk
+++ tests/glxpixmap/glxpixmap.mk
@@ -4,3 +4,5 @@ $(TEST_BUILD_DIR)/glxpixmap: tests/glxpixmap/glxpixmap.c $(LIBGL)
 $(TEST_BUILD_DIR)/glxpixmap_create_destroy: tests/glxpixmap/glxpixmap_create_destroy.c $(LIBGL)
 	$(CC) tests/glxpixmap/glxpixmap_create_destroy.c -Iinclude -I/usr/X11/include -o $(TEST_BUILD_DIR)/glxpixmap_create_destroy $(LINK_TEST)
 
+$(TEST_BUILD_DIR)/glxpixmap_destroy_invalid: tests/glxpixmap/glxpixmap_destroy_invalid.c $(LIBGL)
+	$(CC) tests/glxpixmap/glxpixmap_destroy_invalid.c -Iinclude -I/usr/X11/include -o $(TEST_BUILD_DIR)/glxpixmap_destroy_invalid $(LINK_TEST)
