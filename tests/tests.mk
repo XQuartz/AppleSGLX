36831bd264373a287a3d1b631ee9904d26ea717c
gstaplin@apple.com <gstaplin@apple.com@9052f27e-8171-453d-b9bb-b0e8541e09c8>
2009-02-20 04:17:34 +0000

    Add a shared context test.  It's a test from Mesa by Brian Paul with a compatible license.
    git-svn-id: http://svn.macosforge.org/repository/xquartz/AppleSGLX/trunk@194 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- tests/tests.mk
+++ tests/tests.mk
@@ -15,6 +15,7 @@ include tests/texenv/texenv.mk
 include tests/engine/engine.mk
 include tests/glxpixmap/glxpixmap.mk
 include tests/triangle_glx_single/triangle_glx.mk
+include tests/shared/shared.mk
 
 
 tests: $(TEST_BUILD_DIR)/simple $(TEST_BUILD_DIR)/fbconfigs $(TEST_BUILD_DIR)/triangle_glx \
@@ -26,4 +27,5 @@ tests: $(TEST_BUILD_DIR)/simple $(TEST_BUILD_DIR)/fbconfigs $(TEST_BUILD_DIR)/tr
   $(TEST_BUILD_DIR)/create_destroy_context_with_drawable \
   $(TEST_BUILD_DIR)/create_destroy_context_with_drawable_2 \
   $(TEST_BUILD_DIR)/render_types \
-  $(TEST_BUILD_DIR)/glxpixmap_create_destroy
+  $(TEST_BUILD_DIR)/glxpixmap_create_destroy \
+  $(TEST_BUILD_DIR)/sharedtex
