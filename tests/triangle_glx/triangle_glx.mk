85b64237a673068ea79aa92ffae5854fe2b46ce7
gstaplin@apple.com <gstaplin@apple.com@9052f27e-8171-453d-b9bb-b0e8541e09c8>
2009-03-03 19:51:51 +0000

    Add a torture test for surface creation and destruction.
    You can verify it with:
    env LIBGL_DIAGNOSTIC=1 ./testbuilds/triangle_glx_surface
    
    It doesn't appear that the X server or libGL clients grow
    with the test, so I believe the cleanup is occurring properly.
    
    
    
    git-svn-id: http://svn.macosforge.org/repository/xquartz/AppleSGLX/trunk@277 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- tests/triangle_glx/triangle_glx.mk
+++ tests/triangle_glx/triangle_glx.mk
@@ -1,2 +1,5 @@
 $(TEST_BUILD_DIR)/triangle_glx: tests/triangle_glx/triangle_glx.c $(LIBGL)
 	$(CC) tests/triangle_glx/triangle_glx.c -Iinclude -o $(TEST_BUILD_DIR)/triangle_glx $(LINK_TEST)
+
+$(TEST_BUILD_DIR)/triangle_glx_surface: tests/triangle_glx/triangle_glx_surface.c $(LIBGL)
+	$(CC) tests/triangle_glx/triangle_glx_surface.c -Iinclude -o $(TEST_BUILD_DIR)/triangle_glx_surface $(LINK_TEST)
