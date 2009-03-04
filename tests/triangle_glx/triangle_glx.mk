dd03b91bb7de4788de7c102a4e0ac6bdb68167e3
gstaplin@apple.com <gstaplin@apple.com@9052f27e-8171-453d-b9bb-b0e8541e09c8>
2009-03-04 00:13:42 +0000

    Add an improved version of the surface test.
    This version destroys the window between:
    glXMakeCurrent(dpy, None, ctx); and 
    glXMakeCurrent(dpy, mainwin, ctx);
    
    It seems to be leaking in the client here, but leaks
    is reporting 0 leaks, which would seem to indicate
    we have a list or similar data structure that isn't
    being delinked.
    
    
    git-svn-id: http://svn.macosforge.org/repository/xquartz/AppleSGLX/trunk@281 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- tests/triangle_glx/triangle_glx.mk
+++ tests/triangle_glx/triangle_glx.mk
@@ -3,3 +3,6 @@ $(TEST_BUILD_DIR)/triangle_glx: tests/triangle_glx/triangle_glx.c $(LIBGL)
 
 $(TEST_BUILD_DIR)/triangle_glx_surface: tests/triangle_glx/triangle_glx_surface.c $(LIBGL)
 	$(CC) tests/triangle_glx/triangle_glx_surface.c -Iinclude -o $(TEST_BUILD_DIR)/triangle_glx_surface $(LINK_TEST)
+
+$(TEST_BUILD_DIR)/triangle_glx_surface-2: tests/triangle_glx/triangle_glx_surface-2.c $(LIBGL)
+	$(CC) tests/triangle_glx/triangle_glx_surface-2.c -Iinclude -o $(TEST_BUILD_DIR)/triangle_glx_surface-2 $(LINK_TEST)
