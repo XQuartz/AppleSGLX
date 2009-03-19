85cf8f896b02b8d49ecd6bd3551c45efff318a64
jeremyhu@freedesktop.org <jeremyhu@freedesktop.org@9052f27e-8171-453d-b9bb-b0e8541e09c8>
2009-03-19 01:27:49 +0000

    Better API compatability with OpenGL.framework's gl.h
    git-svn-id: http://svn.macosforge.org/repository/xquartz/AppleSGLX/trunk@315 9052f27e-8171-453d-b9bb-b0e8541e09c8

--- gen_gl_h.sh
+++ gen_gl_h.sh
@@ -2,6 +2,14 @@
 
 {
   cat include/GL/gl.h.header 
-  grep gl.*ProcPtr /System/Library/Frameworks/OpenGL.framework/Headers/gl{,ext}.h | sed 's:^.*\(gl.*Ptr\).*$:\1:' | sort -u | perl -ne 'chomp($_); $s = "PFN".uc($_); $s =~ s/PROCPTR/PROC/; print "#define ".$_." ".$s."\n"'
+  if grep -q GL_GLEXT_PROTOTYPES /System/Library/Frameworks/OpenGL.framework/Headers/gl.h ; then
+    grep gl.*ProcPtr /System/Library/Frameworks/OpenGL.framework/Headers/gl{,ext}.h | sed 's:^.*\(gl.*Ptr\).*$:\1:' | sort -u | perl -ne 'chomp($_); $s = "PFN".uc($_); $s =~ s/PROCPTR/PROC/; print "#define ".$_." ".$s."\n"'
+  else
+    { 
+      echo "#define GL_GLEXT_FUNCTION_POINTERS 1"
+      grep gl.*ProcPtr /System/Library/Frameworks/OpenGL.framework/Headers/gl{,ext}.h | sed 's:^.*\(gl.*Ptr\).*$:\1:' | sort -u | perl -ne 'chomp($_); $s = "PFN".uc($_); $s =~ s/PROCPTR/PROC/; print "#define ".$_." ".$s."\n"'
+      echo '#include "/System/Library/Frameworks/OpenGL.framework/Headers/gl.h"'
+    } | gcc -E - | grep typedef.*PFN
+  fi
   cat include/GL/gl.h.footer
 } > include/GL/gl.h
